8c813af126053abde1cef854633c0454f135e747
Mohammed <Mohammed.94822@cs.mti.edu.eg>
2024-09-14 21:20:36 +0300

    Fix Vite configuration for axios module

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      external: ['axios'] // إضافة axios كـ external module
+      external: [] s
     }
   },
-})
+});
